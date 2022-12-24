165e1e515062f1fac07dab8c3e2ee1b242a9bd97
philica <fikretucker79@gmail.com>
2022-12-24 15:38:25 +0300

    Create previousWork Endpoint get all previousWork get single  previousWork create previousWork delete previousWork update previousWork

--- app/routes/users.routes.js
+++ app/routes/users.routes.js
@@ -84,7 +84,80 @@ router.patch('/:id', getUser, async (req, res) => {
 
 // ======================= previuos work ==================================
 
+//get all previousWork 
+router.get("/previousWork/:id", getUser, async (req,res) => {
+    let userId = req.params.id
+    try{
+      const prevWork = await user.findById(userId,{previousWork:1})
+      res.json(prevWork)
+    }catch(err){
+      res.status(400).json({message: err.message})
+    }
+  })
 
+//get single previousWork
+router.get("/previousWork/:id/:id1", getUser, async (req,res) => {
+    let workId = req.params.id1
+    try{
+      const prevWork = await user.find({"previousWork._id":workId},{previousWork:1})
+      res.json(prevWork)
+    }catch(err){
+      res.status(400).json({message: err.message})
+    }
+  })
+
+//create previousWork 
+router.post("/previousWork/:id", getUser, async (req,res) => {
+    let userId = req.params.id
+    let newPrevWork = {
+      workTitle: req.body.workTitle,
+      workThumbnail: req.body.workThumbnail,
+      workDescription: req.body.workDescription,
+      workLink: req.body.workLink,
+      workSkill: req.body.workSkill,
+    }
+    try{
+      const prevWork = await user.updateOne({_id: userId}, {$push: {previousWork: newPrevWork}})
+      res.status(201).json(newPrevWork)
+  
+    }catch(err){
+      res.status(400).json({message: err.message})
+    }
+  })
+
+//delete previousWork
+router.delete("/previousWork/:id/:id1", getUser, async (req,res) => {
+    let userId = req.params.id
+    let workId = req.params.id1
+    try{
+      const prevWork = await user.updateOne({_id: userId},{$pull: {"previousWork": {_id: workId}}})
+      res.status(201).json({message : "work deleted",prevWork})
+    }catch(err){
+      res.status(400).json({message: err.message})
+    }
+  })
+
+//update previousWork
+router.patch("/previousWork/:id/:id1", getUser, async (req,res) => {
+
+    let userId = req.params.id
+    let workId= req.params.id1
+    let updatedPrevWork = {
+      workTitle: req.body.workTitle || "",
+      workThumbnail: req.body.workThumbnail || "",
+      workDescription: req.body.workDescription || "",
+      workLink: req.body.workLink || "",
+      workSkill: req.body.workSkill || "",
+    }
+  
+    try{
+      const prevWork = await user.updateOne({_id: userId,"previousWork._id":workId},{$set:{"previousWork.$": updatedPrevWork}})
+      res.status(201).json(prevWork)
+  
+    }catch(err){
+      res.status(400).json({message: err.message})
+    }
+  })
 
 // ======================= skill ==================================
 
