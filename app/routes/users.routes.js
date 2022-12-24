be18000cca5bc26f70691d06e3685e02ab88a108
philica <fikretucker79@gmail.com>
2022-12-24 15:08:17 +0300

    create user endpoint get all user get single user create user delete user update user

--- app/routes/users.routes.js
+++ app/routes/users.routes.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const { isObjectIdOrHexString } = require('mongoose')
 const router = express.Router()
 const user = require('../models/user.model')
 
 //set list of routes 
+
+
+// ======================= User ==================================
 
 //get all users 
 router.get('/', async(req,res) => {
@@ -15,25 +19,106 @@ router.get('/', async(req,res) => {
 })
 
 //get single user 
-
+router.get('/:id', getUser, (req, res) => {
+    res.send(res.user.userName)
+  })
+  
 //create user 
+router.post('/', async(req, res) => {
+    const newUser = new user({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      userName: req.body.userName,
+      password: req.body.password,
+      email: req.body.email,
+      phoneNo: req.body.phoneNo,
+      gender: req.body.gender,
+      profilePhoto: req.body.profilePhoto,
+      location: req.body.location,
+      title: req.body.title,
+      titleOverview: req.body.titleOverview,
+      previousWork: req.body.previousWork,
+      skill: req.body.skill,
+      language: req.body.language,
+      education: req.body.education,
+      certification: req.body.certification,
+      employmentHistory: req.body.employmentHistory,
+      otherExperience: req.body.otherExperience
+  
+    })
+    try{
+      const newInstance = await newUser.save()
+      res.status(201).json(newUser)  
+    }catch(err){
+      res.status(400).json({message: err.message})
+    }
+    
+  })
 
 //delete user 
+router.delete('/:id', getUser, async (req, res) => {
+    try{
+      await res.user.remove()
+      res.json({message: `Deleted ${res.user.userName}` })
+    }catch(err){
+      res.status(400).json({message: err.message})
+    }
+  })
+
 
 //update user 
+router.patch('/:id', getUser, async (req, res) => {
+    if(req.body.userName != null) {
+      //update title field from the returned json 
+      res.user.title = req.body.title
+    }
+    try{
+        const updatedUser = await res.user.save()
+      res.json(updatedUser)
+    }catch(err){
+      res.status(400).json({message: err.message})
+    }
+})
 
-//update user previous work
 
-//update user skill
 
-//update user language
+// ======================= previuos work ==================================
 
-//update user education
 
-//update user certification
 
-//update user employment history
+// ======================= skill ==================================
 
-//update other exprience 
+
+// ======================= language ==================================
+
+
+// ======================= education ==================================
+
+
+// ======================= education ==================================
+
+
+// ======================= employment history ==================================
+
+
+// ======================= other exprience  ==================================
+
+
+//create a middleware to check if the user exists
+
+async function getUser(req, res, next){
+    let returnedUser
+    try{
+      returnedUser = await user.findById(req.params.id)
+      if(user == null){
+        return res.status(404).json({message:"can not found user"})
+      }
+    }catch(err){
+      return res.status(500).json({message:err.message})
+    }
+  
+    res.user = returnedUser
+    next()
+  }
 
 module.exports = router
