21a89e163da8f70087241b7f6c1a8fdc06ec320d
natyyG <natnalecs16@gmail.com>
2022-12-28 04:57:01 -0800

    Blog filtering modification

--- FrontEnd/src/components/Blogsidebar.jsx
+++ FrontEnd/src/components/Blogsidebar.jsx
@@ -23,6 +23,7 @@ const Blogsidebar = ({ catagoryFiletering }) => {
       <div class="sidebar-header widget">
         <h4>Catagories</h4>
         <ul class="catlinks">
+          <li onClick={() => {catagoryFiletering("all")}}>All</li>
           <li onClick={() => { catagoryFiletering("webdevelopment") }}>Web-Developer</li>
           <li onClick={() => { catagoryFiletering("marketing") }}>Marketing</li>
           <li onClick={() => { catagoryFiletering("tech") }}>Technology</li>
