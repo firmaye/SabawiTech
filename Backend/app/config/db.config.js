dd0d814ea3cce630b13e6a15fee8d1e7914e0d49
AlazarG19 <alazargetachew70@gmail.com>
2022-12-30 11:42:05 -0800

    db

--- Backend/app/config/db.config.js
+++ Backend/app/config/db.config.js
@@ -1,6 +1,3 @@
 module.exports = {
     url: "mongodb+srv://philica:sabifithawok@cluster0.1c5jn.mongodb.net/Sabawi?retryWrites=true&w=majority"
 }
-module.exports = {
-    url: "mongodb+srv://philica:sabifithawok@cluster0.1c5jn.mongodb.net/Sabawi?retryWrites=true&w=majority"
-}
