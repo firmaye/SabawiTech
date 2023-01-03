e8f7bb8e0fe3fc076bbf894b13fbab43a20d80d4
philica <fikretucker79@gmail.com>
2023-01-03 11:52:35 +0300

    Add feature to enable photo on Previous work(portfolio) part

--- Backend/app/controllers/userController/previousWork.controller.js
+++ Backend/app/controllers/userController/previousWork.controller.js
@@ -1,5 +1,19 @@
 const db = require("../../models");
 const user = db.users;
+const multer = require('multer')
+
+//storage 
+const Storage = multer.diskStorage({
+  destination:'uploads/image',
+  filename:(req,file,cb) => {
+     cb(null, Date.now() + file.originalname )
+  }
+})
+
+const upload = multer({
+  storage: Storage 
+}).single('workThumbnail')
+
 
 // Retrieve single user from the database.
 exports.getAll = async (req,res) => {
@@ -23,23 +37,46 @@ exports.getSingle = async (req,res) => {
   };
 
 // create single PrevWork 
-exports.create = async (req,res) => {
+exports.create = (req,res) => {
+  let userId = req.params.id
+  let imageName = ""
+
+   upload(req,res,(err) => {
+    if(err){
+      console.log(err)
+    }
+    else{
+      console.log("filename: "+req.file.filename)
+      imageName = req.file.filename
+      console.log("imageName: "+imageName)
+    }
+   })
+
+
+   setTimeout(() => {
     let userId = req.params.id
     let newPrevWork = {
       workTitle: req.body.workTitle,
-      workThumbnail: req.file.filename,
+      workThumbnail: imageName,
       workDescription: req.body.workDescription,
       workLink: req.body.workLink,
-      workSkill: req.body.workSkill,
+      workSkill:req.body.workSkill
     }
     try{
-      const prevWork = await user.updateOne({_id: userId}, {$push: {previousWork: newPrevWork}})
-      res.status(201).json(newPrevWork)
+      const prevWork = user.updateOne(
+        {_id: userId}, 
+        {$push: {previousWork: newPrevWork}}
+        ).then(() => {
+          res.send({success:true})
+        }).catch((err) => console.log(err))
   
     }catch(err){
       res.status(400).json({message: err.message})
     }
-  };
+  }, "1000")
+  
+   
+}
 
 
 
