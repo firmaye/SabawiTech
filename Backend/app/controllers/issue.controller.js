325d9620763453e343d49543d51544c98d3585c6
AlazarG19 <alazargetachew70@gmail.com>
2023-01-04 21:46:23 -0800

    reversing change of issues.controller

--- Backend/app/controllers/issue.controller.js
+++ Backend/app/controllers/issue.controller.js
@@ -2,11 +2,9 @@ const db = require("../models");
 const EmailSender = require("../helpers/sendEmail.js");
 // Create and Save a new issue
 exports.post = async (req, res) => {
-    console.log(req.body)
     const name = req.body.name
     const email = req.body.email
     const subject = req.body.subject
     const message = req.body.message
-    console.log("start sending")
     EmailSender({ email, name, subject, message })
 };
